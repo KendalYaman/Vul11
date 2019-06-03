5a9fee6855ba65258fc6cef823e621ddf906a310
Nraxa <nraxa@MacBook-Pro-de-Nraxa.local>
2019-06-03 21:20:57 +0200

    Lol

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 TARGET_BIN="/home/user/lab/Vul11/build"
 
-afl-fuzz -i ./build.afl/input/ -f ./build.afl/generated/board.map -o ./build.afl/findings -- $TARGET_BIN ./build.afl/generated/board
+afl-fuzz -i ./build.afl/input/ -f ./build.afl/generated/board.map -o ./build.afl/findings -- $TARGET_BIN ./build.afl/generated/board.map
